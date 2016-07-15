55b630b5ab60b2b5a34e538a157515491d58a1a0
陆欣然 <luxinran@uzoo.cn>
2016-07-15 21:35:11 +0800

    add computing errors module

--- src/example.cpp
+++ src/example.cpp
@@ -1,5 +1,8 @@
-#include "MoveSenseCamera.h"
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
 #include <ros/ros.h>
+#include "MoveSenseCamera.h"
 // PCL specific includes
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl/point_cloud.h>
@@ -7,20 +10,19 @@
 #include <pcl/point_types.h>
 #include <pcl/filters/voxel_grid.h>
 #include <pcl_ros/point_cloud.h>
-
-// c++
-#include <iostream>
-#include <stdio.h>
-
-// opencv
+#include <pcl/ModelCoefficients.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+// opencv specific includes
 #include <cv_bridge/cv_bridge.h>
 #include "opencv2/opencv.hpp"
-//#include <imgcodecs/imgcodecs.hpp>
-//#include <core/core.hpp>
-//#include <highgui/highgui.hpp>
 #include "opencv2/imgcodecs/imgcodecs.hpp"
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
+
+#define K 50
 
 int
 main (int argc, char** argv)
@@ -43,42 +45,46 @@ main (int argc, char** argv)
 
   unsigned char * img_data  = new unsigned char[len];
 
-  pcl::PointCloud<pcl::PointXYZ>::Ptr msg (new pcl::PointCloud<pcl::PointXYZ>);
-  msg->header.frame_id = "map";
-  msg->height = height;
-  msg->width = width;
-  msg->is_dense = false;
-  msg->points.resize(msg->width * msg->height);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+  cloud->header.frame_id = "map";
+  cloud->height = height;
+  cloud->width = width;
+  cloud->is_dense = false;
+  cloud->points.resize(cloud->width * cloud->height);
+
+  pcl::PointCloud<pcl::PointXYZ>::Ptr fit_cloud (new pcl::PointCloud<pcl::PointXYZ>);
+  fit_cloud->height = K*2;
+  fit_cloud->width = K*2;
+  fit_cloud->is_dense = true;
+  fit_cloud->points.resize(fit_cloud->width * fit_cloud->height);
 
   ros::Rate loop_rate(4);
   while (nh.ok())
-  {
-    // for (size_t i = 0; i < msg->points.size (); ++i)
-    //   {
-    // 	msg->points[i].x = 1024 * rand () / (RAND_MAX + 1.0f);
-    // 	msg->points[i].y = 1024 * rand () / (RAND_MAX + 1.0f);
-    // 	msg->points[i].z = 1024 * rand () / (RAND_MAX + 1.0f);
-    //   }
-    
+  { 
     c.GetImageData(img_data ,len);
       if(len > 0)
 	{
-	  cv::Mat left(height,width,CV_8UC1),disp(height,width,CV_8UC1);
+	  cv::Mat left(height,width,CV_8UC1),disp(height,width,CV_8UC1); // disp is the disparity map
 	  for(int i = 0 ; i < height; i++)
 	    {
 	      memcpy(left.data+width*i,	img_data+(2*i)*width,width);
 	      memcpy(disp.data+width*i,img_data+(2*i+1)*width,width);
 	    }
 	  cv::imshow("left",left);
+	  cv::imshow("disp",disp);
 
-	  double b_multipy_f = 35981.122607;  //z = b*f/d,b_multipy_f = b*f(only 6 number)
+	  //use mid-filter for disp
+	  //cv::medianBlur(disp, disp, 5);
+
+	  double b_multipy_f = 35981.122607;  //z = b*f/d,b_multipy_f = b*f
 	  double f = 599.065803;
+	  double cu = 369.703644;
+	  double cv = 223.365112;
+	  
 	  double x = 0.00;
 	  double y = 0.00;
 	  double z = 0.00;
 	  double d_real = 0.0;
-	  double cu = 369.703644;
-	  double cv = 223.365112;
 
 	  for(int u = 0; u < height; ++u)
 	    for(int v = 0; v < width; ++v)
@@ -86,7 +92,7 @@ main (int argc, char** argv)
 	  	int d = disp.at<uchar>(u,v);
 	  	if(d<32*4)
 	  	  {
-	  	    d_real = disp.at<uchar>(u,v)/4;
+	  	    d_real = d/4.0;
 	  	  }
 	  	else
 	  	  {
@@ -96,21 +102,69 @@ main (int argc, char** argv)
 	  	x = (u - cu) * z / f;
 	  	y = (v - cv) * z / f;
 
-	  	// save (x,y,z) in pcd
-	  	msg->points[u*disp.cols+v].x = x/255.0f;
-	  	msg->points[u*disp.cols+v].y = y/255.0f;
-	  	msg->points[u*disp.cols+v].z = z/255.0f;
+		//if (z < 3000)
+		//{
+		// show in rviz
+		cloud->points[u*disp.cols+v].x = x/255.0f;
+		cloud->points[u*disp.cols+v].y = y/255.0f;
+		cloud->points[u*disp.cols+v].z = z/255.0f;
+		// }
 	      }
-	  //pcl::io::savePCDFileASCII ("test_pcd.pcd", *msg);
 	  
-	  cv::imshow("disp",disp);
-	  
-	  char key = cv::waitKey(10);
+	  char key = cv::waitKey(100);
 	  if(key == 'q')
 	    break;
+	  else if (key == 's') // use this frame to test camera
+	    {
+	      // save the image left and disp
+	      cv::imwrite( "left.jpg", left );
+	      cv::imwrite( "disp.jpg", disp );
+	      // select K*K pixels around the central pixel
+	      int index = 0;
+	      for (int u = height/2-K; u < height/2+K; ++u)
+		for (int v = width/2-K; v < width/2+K; ++v)
+		  {
+		    fit_cloud->points[index].x = cloud->points[u*disp.cols+v].x * 255.0f;
+		    fit_cloud->points[index].y = cloud->points[u*disp.cols+v].y * 255.0f;
+		    fit_cloud->points[index].z = cloud->points[u*disp.cols+v].z * 255.0f;
+		    index++;
+		  }
+	      // fitting a plane(use point clouds)
+	      pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+	      pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+	      // Create the segmentation object
+	      pcl::SACSegmentation<pcl::PointXYZ> seg;
+	      // Optional
+	      seg.setOptimizeCoefficients (true);
+	      // Mandatory
+	      seg.setModelType (pcl::SACMODEL_PLANE);
+	      seg.setMethodType (pcl::SAC_RANSAC);
+	      seg.setDistanceThreshold (0.01);
+	      
+	      seg.setInputCloud (fit_cloud);
+	      seg.segment (*inliers, *coefficients);
+
+	      // print the coefficients of the plane
+	      double pa = coefficients->values[0];
+	      double pb = coefficients->values[1];
+	      double pc = coefficients->values[2];
+	      double pd = coefficients->values[3];
+	      std::cout << pa << " " << pb << " " << pc << " " << pd << std::endl;
+	      // compute errors
+	      double error = 0.00;
+	      for (int i = 0; i < 4*K*K; ++i)
+		{
+		  error += pow(pa * fit_cloud->points[index].x + pb * fit_cloud->points[index].y + pc * fit_cloud->points[index].z + pd, 2) / 1e+12;
+		}
+	      error /=  (pa * pa + pb * pb + pc * pc);
+	      std::cout << "error = " << error << std::endl;
+	      // show z-axis distance
+	      double distance = -coefficients->values[3];
+	      std::cout << "distance = " << distance << " cm" << std::endl;
+	    }
 	}
-    pcl_conversions::toPCL(ros::Time::now(), msg->header.stamp);
-    pub.publish (msg);
+    pcl_conversions::toPCL(ros::Time::now(), cloud->header.stamp);
+    pub.publish (cloud);
     ros::spinOnce ();
     loop_rate.sleep ();
   }
