292e2508b5f692aa712127478ee7d7b5c1ffce78
陆欣然 <luxinran@uzoo.cn>
2016-07-18 15:01:54 +0800

    modify something and add draw rect

---
+++ src/draw_curve.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pylab as pl
+x = np.linspace(0, 20, 100)
+x = [2.775,1.336,1.283,0.999,0.853,0.746,0.642, 0.584] # x values
+y = [21.66,4.58,2.69,1.9254,1.489,1.067,0.8409, 0.446] # y values
+pl.plot(x, y, 'o') # draw points
+pl.show()
+
+# http://blog.csdn.net/ywjun0919/article/details/8692018
--- src/example.cpp
+++ src/example.cpp
@@ -22,7 +22,68 @@
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
 
-#define K 50
+#define K 30
+
+void fit_plane(int height, int width,  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud,  pcl::PointCloud<pcl::PointXYZ>::Ptr fit_cloud)
+{
+  // save the image left and disp
+  //cv::imwrite( "left.jpg", left );
+  //cv::imwrite( "disp.jpg", disp );
+  // select K*K pixels around the central pixel
+
+  int index = 0;
+  for (int u = height/2-K; u < height/2+K; ++u)
+    for (int v = width/2-K; v < width/2+K; ++v)
+      {
+	fit_cloud->points[index].x = cloud->points[u*width+v].x * 255.0f;
+	fit_cloud->points[index].y = cloud->points[u*width+v].y * 255.0f;
+	fit_cloud->points[index].z = cloud->points[u*width+v].z * 255.0f;
+	index++;
+      }
+  // fitting a plane(use point clouds)
+  pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+  pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+  // Create the segmentation object
+  pcl::SACSegmentation<pcl::PointXYZ> seg;
+  // Optional
+  seg.setOptimizeCoefficients (true);
+  // Mandatory
+  seg.setModelType (pcl::SACMODEL_PLANE);
+  seg.setMethodType (pcl::SAC_RANSAC);
+  seg.setDistanceThreshold (0.01);
+	      
+  seg.setInputCloud (fit_cloud);
+  seg.segment (*inliers, *coefficients);
+
+  // print the coefficients of the plane
+  double pa = coefficients->values[0];
+  double pb = coefficients->values[1];
+  double pc = coefficients->values[2];
+  double pd = coefficients->values[3];
+	      
+  // compute errors
+  double error = 0.00;
+  double ave = 0.00;
+  for (int i = 0; i < 4*K*K; ++i)
+    {
+      error += abs(pa * fit_cloud->points[i].x + pb * fit_cloud->points[i].y + pc * fit_cloud->points[i].z + pd);
+      ave += abs(pa * fit_cloud->points[i].x + pb * fit_cloud->points[i].y + pc * fit_cloud->points[i].z);
+    }
+  ave /= (4 * K * K);
+  error /= sqrt(pa * pa + pb * pb + pc * pc);
+  error /= (4 * K * K);	      
+  double distance = -coefficients->values[3];
+	      
+  if (abs(ave - distance) <= 5)
+    {
+      std::cout << pa << " " << pb << " " << pc << " " << pd << std::endl;
+      std::cout << "error = " << error << " mm  ";
+      std::cout << "ave = " << ave << " mm  ";
+      // show z-axis distance
+      std::cout << "distance = " << distance << " mm" << std::endl;
+    }
+	    
+}
 
 int
 main (int argc, char** argv)
@@ -59,6 +120,9 @@ main (int argc, char** argv)
   fit_cloud->points.resize(fit_cloud->width * fit_cloud->height);
 
   ros::Rate loop_rate(4);
+
+  bool flag = false;
+  
   while (nh.ok())
   { 
     c.GetImageData(img_data ,len);
@@ -70,8 +134,16 @@ main (int argc, char** argv)
 	      memcpy(left.data+width*i,	img_data+(2*i)*width,width);
 	      memcpy(disp.data+width*i,img_data+(2*i+1)*width,width);
 	    }
+	  // draw a rec(2K * 2K) in the middle of the image "left"
+	  cv::rectangle(
+			left,
+			//cv::Point(height/2-K, width/2-K),
+			//cv::Point(height/2+K, width/2+K),
+			cv::Rect(width/2-K, height/2-K, 2*K, 2*K),
+			cv::Scalar(255, 255, 255)
+			);
 	  cv::imshow("left",left);
-	  cv::imshow("disp",disp);
+	  //cv::imshow("disp",disp);
 
 	  //use mid-filter for disp
 	  //cv::medianBlur(disp, disp, 5);
@@ -105,62 +177,27 @@ main (int argc, char** argv)
 		//if (z < 3000)
 		//{
 		// show in rviz
-		cloud->points[u*disp.cols+v].x = x/255.0f;
+		cloud->points[u*disp.cols+v].x = x/255.0f; // /255.0f is just for show in rviz, should be deleted later
 		cloud->points[u*disp.cols+v].y = y/255.0f;
 		cloud->points[u*disp.cols+v].z = z/255.0f;
 		// }
 	      }
+
+	  if (flag == true)
+	    {
+	      fit_plane(height, width, cloud, fit_cloud);
+	    }
 	  
 	  char key = cv::waitKey(100);
 	  if(key == 'q')
 	    break;
-	  else if (key == 's') // use this frame to test camera
+	  else if (key == 'c')
 	    {
-	      // save the image left and disp
-	      cv::imwrite( "left.jpg", left );
-	      cv::imwrite( "disp.jpg", disp );
-	      // select K*K pixels around the central pixel
-	      int index = 0;
-	      for (int u = height/2-K; u < height/2+K; ++u)
-		for (int v = width/2-K; v < width/2+K; ++v)
-		  {
-		    fit_cloud->points[index].x = cloud->points[u*disp.cols+v].x * 255.0f;
-		    fit_cloud->points[index].y = cloud->points[u*disp.cols+v].y * 255.0f;
-		    fit_cloud->points[index].z = cloud->points[u*disp.cols+v].z * 255.0f;
-		    index++;
-		  }
-	      // fitting a plane(use point clouds)
-	      pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
-	      pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
-	      // Create the segmentation object
-	      pcl::SACSegmentation<pcl::PointXYZ> seg;
-	      // Optional
-	      seg.setOptimizeCoefficients (true);
-	      // Mandatory
-	      seg.setModelType (pcl::SACMODEL_PLANE);
-	      seg.setMethodType (pcl::SAC_RANSAC);
-	      seg.setDistanceThreshold (0.01);
-	      
-	      seg.setInputCloud (fit_cloud);
-	      seg.segment (*inliers, *coefficients);
-
-	      // print the coefficients of the plane
-	      double pa = coefficients->values[0];
-	      double pb = coefficients->values[1];
-	      double pc = coefficients->values[2];
-	      double pd = coefficients->values[3];
-	      std::cout << pa << " " << pb << " " << pc << " " << pd << std::endl;
-	      // compute errors
-	      double error = 0.00;
-	      for (int i = 0; i < 4*K*K; ++i)
-		{
-		  error += pow(pa * fit_cloud->points[index].x + pb * fit_cloud->points[index].y + pc * fit_cloud->points[index].z + pd, 2) / 1e+12;
-		}
-	      error /=  (pa * pa + pb * pb + pc * pc);
-	      std::cout << "error = " << error << std::endl;
-	      // show z-axis distance
-	      double distance = -coefficients->values[3];
-	      std::cout << "distance = " << distance << " cm" << std::endl;
+	      flag = false;
+	    }
+	  else if (key == 'o') // use this frame to test camera
+	    {
+	      flag = true;
 	    }
 	}
     pcl_conversions::toPCL(ros::Time::now(), cloud->header.stamp);
