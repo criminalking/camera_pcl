8df5995b8928bd0de8cebf4c528c1f3b08908c3a
陆欣然 <luxinran@uzoo.cn>
2016-07-19 11:15:51 +0800

    modify draw_curve

--- src/draw_curve.py
+++ src/draw_curve.py
@@ -2,17 +2,26 @@
 
 import numpy as np
 import pylab as pl
-f = open("../out.txt")
-x = []
-y = []
-for line in f:
-    info = line.split()
-    y.append(float(info[0]))
-    x.append(float(info[1]))
 
-results = {}
-coeffs = np.polyfit(x, y, 2)
-results = coeffs.tolist()
-p = np.poly1d(coeffs)                  
-pl.plot(x,y,'o',x,p(x))
-pl.show()
+def main():
+    f = open("../out.txt")
+    x = []
+    y = []
+    for line in f:
+        info = line.split()
+        y.append(float(info[0]))
+        x.append(float(info[1]))
+        
+    results = {}
+    coeffs = np.polyfit(x, y, 2)
+    results = coeffs.tolist()
+    p = np.poly1d(coeffs)
+
+    pl.plot(x, y, 'or', x, p(x), 'b')
+    pl.xlabel("distance/mm")
+    pl.ylabel("error/mm")
+    pl.title("Precision of the binocular camera")
+    pl.show()
+
+if __name__=="__main__":
+    main()
